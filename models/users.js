cfbcb58714fe9eafa638bd0af81374fbc2b510b6
Seha Gulnar Dalay <sehagulnar@gmail.com>
2018-05-28 11:37:30 -0400

    first commit

---
+++ models/users.js
@@ -0,0 +1,48 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  questions: {
+    type: Object
+  }
+});
+
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashPassword = function (password) {
+  return bcrypt.hash(password, 10);
+};
+
+userSchema.methods.serialize = function () {
+  return {
+    id: this._id,
+    username: this.username || '',
+    firstName: this.firstName || '',
+    lastName: this.lastName || ''
+  };
+};
+
+
+
+const User = mongoose.model('User', userSchema);
+module.exports = { User };
